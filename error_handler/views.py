541ea4b908d8522f324def5497e6245aeb72acb6
NamanJain1902 <1902ben10@gmail.com>
2021-08-24 12:27:57 +0530

    corrected status in logs

--- error_handler/views.py
+++ error_handler/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def permission_denied(request, exception):
-    raise PermissionDenied
+    raise render(request, "error_handler/permission_denied.html", status=403)
 
 def page_not_found(request, exception):
-    return render(request, "error_handler/page_not_found.html", {})
+    return render(request, "error_handler/page_not_found.html", status=404)
 
 def bad_request(request, exception):
-    return render(request, "")
+    return render(request, "error_handler/bad_request.html", status=400)
 
 def server_error(request, exception=None):
-    return render(request, "error_handler/server_error.html")
+    return render(request, "error_handler/server_error.html", status=500)
